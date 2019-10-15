e01d7a0ab32a24a190eeb6a343bef71d50ae676c
Juan <Juan.Paggi@sdc.com.ar>
2019-10-15 17:12:35 -0300

    Se genero el servicio de Tags para noticias - Frontend
    Se encontro un bug en put noticias

--- frontend/src/app/services/noticias.service.ts
+++ frontend/src/app/services/noticias.service.ts
@@ -4,7 +4,7 @@ import { NoticiasDto } from '../providers/dto/NoticiasDto';
 import { Observable } from 'rxjs';
 import { NoticiaItem } from '../providers/entities/NoticiaItem.entity';
 import { environment } from 'src/environments/environment';
-import { NoticiaByIdDto } from '../providers/dto/noticiaByIdDTO';
+import { NoticiaByIdDto } from '../providers/dto/noticiaByIdDto';
 import { CrearNoticiaDto } from '../providers/dto/CreateNoticiaDto';
 
 @Injectable({
---
+++ frontend/src/app/services/tags.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TagsDto } from '../providers/dto/TagsDto';
+import { TagItem } from '../providers/entities/TagItem.entity';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TagsService {
+
+  constructor(private http: HttpClient) { }
+
+  public getAllTags(body: TagsDto): Observable<TagItem[]> {
+    let headers = {};
+    return this.http.get<TagItem[]>(
+        environment.apiEndpoint + '/tags',
+        headers
+    );
+  }
+  
+}
